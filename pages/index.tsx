9dcf15814ecb1c8ebaa21de3cecddb927ca87e64
Jan Kaifer <jan@kaifer.cz>
2023-01-17 15:45:50 +0100

    added bootstrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div
-      className="class-name"
+      className="class-name alert alert-info"
       style={{
         height: 100,
         width: 100,
