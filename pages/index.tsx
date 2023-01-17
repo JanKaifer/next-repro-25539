ce445729e48f04f6ed64c75bd06ec56ed8fe3d7a
Jan Kaifer <jan@kaifer.cz>
2023-01-17 15:41:58 +0100

    created reproduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <div
+      className="class-name"
+      style={{
+        height: 100,
+        width: 100,
+      }}
+    ></div>
+  );
 }
